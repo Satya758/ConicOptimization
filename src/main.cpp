8a8aacc644747dacdbfab7821e3d3326f3c3174c
Satya <satya.love@gmail.com>
2014-05-04 16:20:00 +0530

    --> Initial CMAKE Project file commit, CMAKE hello world project

---
+++ src/main.cpp
@@ -0,0 +1,18 @@
+/*!
+ * Tests sample data
+ * FIXME Replace all these test samples and file with Google test  
+ */
+
+
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char **argv) {
+  
+  cout << "First file" << endl;
+  return 0;
+}
+
+
+
